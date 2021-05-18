6c6659e54bc83ad8c0e82e12410d22e472fbbb11
seraogianluca <g.serao@studenti.unipi.it>
2021-05-18 15:52:17 +0200

    Authenticated encryption functions

--- include/crypto.h
+++ include/crypto.h
@@ -9,10 +9,15 @@ using namespace std;
 
 class Crypto {
     private:
+        // Controllare che non restino salvate da qualche parte altrimenti non è sicura!!
+        unsigned char *session_key;
 
     public:
-        Crypto() {};
+        Crypto() {
+
+        };
         //EVP_PKEY* readPrivateKey(string pwd);
         unsigned char* generateNonce();
-
+        int encryptMessage(unsigned char *msg, unsigned char *ciphr_msg, unsigned char *tag);
+        int decryptMessage(unsigned char *ciphr_msg, unsigned char *msg);
 };
