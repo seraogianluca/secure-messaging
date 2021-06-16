7b4fa99adf4dca8c0043c355628ca4f2ff11da47
lorepas <pascolorenzo@gmail.com>
2021-06-16 13:38:20 +0200

    server RTT protocol

--- include/server.h
+++ include/server.h
@@ -1,10 +1,7 @@
 #include <fstream>
 #include <sstream>
-#include <fstream>
 #include <iterator>
-#include <vector>
-#include <array>
-#include "crypto.h"
+#include "utils.h"
 #include "socket.h"
 
 
@@ -17,15 +14,20 @@ struct onlineUser {
 struct activeChat {
     onlineUser a;
     onlineUser b;
+
+    activeChat(onlineUser an, onlineUser bn){
+        a = an;
+        b = bn;
+    }
 };
 
-struct serverContext {
+struct ServerContext {
     vector<onlineUser> onlineUsers;
     vector<activeChat> activeChats;
     SocketServer *serverSocket;
     Crypto *crypto;
 
-    serverContext() {
+    ServerContext() {
         serverSocket = new SocketServer(SOCK_STREAM);
         crypto = new Crypto();
     }
@@ -69,6 +71,15 @@ struct serverContext {
         throw runtime_error("Chat not found.");
     }
 
+    bool isUserChatting(string username){
+        for(activeChat chat: activeChats) {
+            if(chat.a.username.compare(username) == 0 || chat.a.username.compare(username) == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
     onlineUser getUser(string username){
         for (onlineUser user : onlineUsers) {
             if(username.compare(user.username) == 0) {
@@ -109,6 +120,40 @@ void send(SocketServer socket, int sd, vector<unsigned char> &buffer) {
     buffer.clear();
 }
 
+void receive(SocketServer *socket, Crypto *crypto, onlineUser sender, vector<unsigned char> &buffer) {
+    unsigned char opCode;
+    try {
+        receive(socket, sender.sd, buffer);
+        opCode = buffer.at(0);
+        buffer.erase(buffer.begin());
+        decrypt(crypto, sender.key_pos, buffer);
+        if(buffer.at(0) != opCode) {
+            cout << "Message tampered" << endl;
+            throw runtime_error("Message tampered");
+        }
+    } catch(const exception& e) {
+        throw;
+    }
+}
+
+void send(SocketServer *socket, Crypto *crypto, onlineUser receiver, vector<unsigned char> &buffer) {
+    unsigned char opCode;
+    try {
+        opCode = buffer.at(0);
+        encrypt(crypto, receiver.key_pos, buffer);
+        buffer.insert(buffer.begin(), opCode);
+        send(socket, receiver.sd, buffer);
+    } catch(const exception& e) {
+        throw;
+    }
+}
+
+void forward(ServerContext ctx, onlineUser sender, onlineUser receiver){
+    vector<unsigned char> buffer;
+    receive(ctx.serverSocket, ctx.crypto, sender, buffer);
+    send(ctx.serverSocket, ctx.crypto, receiver, buffer);
+}
+
 // Utility
 unsigned int readPassword(unsigned char* username, unsigned int usernameLen, unsigned char* password) {
     ifstream file("./resources/credentials.txt");
@@ -131,4 +176,90 @@ unsigned int readPassword(unsigned char* username, unsigned int usernameLen, uns
         }
     }
     return 0;
+}
+
+void requestToTalk(ServerContext ctx, vector<unsigned char> msg, onlineUser sender){
+    array<unsigned char, MAX_MESSAGE_SIZE> tempBuffer;
+    array<unsigned char, NONCE_SIZE> nonce;
+    vector<unsigned char> buffer;
+    unsigned int tempBufferLen = 0;
+    string usernameB;
+    onlineUser receiver;
+    EVP_PKEY *pubKeyB = NULL;
+    EVP_PKEY *pubKeyA = NULL;
+    
+    try {
+        // Receive M1 FROM A
+        msg.erase(msg.begin()); //delete the OPCODE in clear
+        decrypt(ctx.crypto, sender.key_pos, msg);
+        if(msg.at(0) != OP_REQUEST_TO_TALK){
+            errorMessage("Error in the initialization of the RTT", buffer);
+            send(ctx.serverSocket, ctx.crypto, sender, buffer);
+            return;
+        }
+        msg.erase(msg.begin());
+        usernameB = extract(msg);
+        if(ctx.isUserChatting(usernameB)){
+            errorMessage("User is busy", buffer);
+            send(ctx.serverSocket, ctx.crypto, sender, buffer);
+            return;
+        }
+        receiver = ctx.getUser(usernameB);
+        extract(msg, nonce);
+
+        // Send M2 TO B
+        buffer.push_back(OP_REQUEST_TO_TALK);
+        append(sender.username, buffer);
+        append(nonce, buffer);
+        send(ctx.serverSocket, ctx.crypto, receiver, buffer);
+
+        // Receive M3 FROM B
+        receive(ctx.serverSocket, ctx.crypto, receiver, buffer);
+        if(buffer.at(0) == OP_ERROR){
+            cout<<"Request to talk refused"<<endl;
+            buffer.erase(buffer.begin());
+            send(ctx.serverSocket, ctx.crypto, receiver, buffer);
+            return;
+        } else if(buffer.at(0) != OP_REQUEST_TO_TALK){
+            errorMessage("Error in the initialization of the RTT", buffer);
+            send(ctx.serverSocket, ctx.crypto, receiver, buffer);
+            return;
+        }
+
+        //Send M4 TO A
+        ctx.crypto->readPublicKey(usernameB, pubKeyB);
+        tempBufferLen = ctx.crypto->serializePublicKey(pubKeyB, tempBuffer.data());
+        append(tempBuffer, tempBufferLen, buffer);
+        send(ctx.serverSocket, ctx.crypto, sender, buffer);
+
+        // Receive M5 FROM A
+        receive(ctx.serverSocket, ctx.crypto, sender, buffer);
+        if(buffer.at(0) != OP_ERROR){
+            errorMessage("Error in request to talk", buffer);
+            cout<<"Error in request to talk"<<endl;
+            send(ctx.serverSocket, ctx.crypto, receiver, buffer);
+            errorMessage("Error in request to talk", buffer);
+            send(ctx.serverSocket, ctx.crypto, receiver, buffer);
+            return;
+        }
+
+        //Send M6 TO B
+        ctx.crypto->readPublicKey(sender.username, pubKeyA);
+        tempBufferLen = ctx.crypto->serializePublicKey(pubKeyA, tempBuffer.data());
+        append(tempBuffer, tempBufferLen, buffer);
+        send(ctx.serverSocket, ctx.crypto, receiver, buffer);
+
+        //Receive M7 FROM B AND FORWORD TO A
+        receive(ctx.serverSocket, ctx.crypto, receiver, buffer);
+        if(buffer.at(0) == OP_REQUEST_TO_TALK){
+            send(ctx.serverSocket, ctx.crypto, sender, buffer);
+            activeChat a(sender, receiver);
+            ctx.activeChats.push_back(a);
+        } else {
+            send(ctx.serverSocket, ctx.crypto, sender, buffer);
+            cout<<"Error during the finalizing of the request to talk"<<endl;
+        }
+    } catch(const exception& e) {
+        throw;
+    }  
 }
